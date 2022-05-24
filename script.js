061c242cfd2e10b85a3eea57abd6772e9e829493
Jessie Chen <notionj.jc@gmail.com>
2022-05-24 16:18:13 -0400

    Fix counter for adding the most recent book, add delete button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const bookDisplay = document.getElementById("book-display");
 
 let myLibrary = [];
 
+
 function Book(title, author, pages, progress){
     this.title = title;
     this.author = author;
@@ -17,37 +18,35 @@ function addBookToLibrary(){
     author = document.getElementById("author").value;
     pages = document.getElementById("pages").value;
     progress = document.getElementById("progress").value;
-    const newBook = new Book(title, author, pages, progress);
-    myLibrary.push(newBook);
-    
-    
-    
-    for (i = 0; i < myLibrary.length; i++){
-        let counter = 0;
-        if (i == counter){
-            const bookContainer = document.createElement("div");
-            bookContainer.setAttribute("class", "books");
-            const bookTitle = document.createElement("p");
-            const bookAuthor = document.createElement("p");
-            const bookPages = document.createElement("p");
-            const bookStatus = document.createElement("p");
-            bookTitle.append(myLibrary[i][`title`]);
-            bookAuthor.append(myLibrary[i][`author`]);
-            bookPages.append(myLibrary[i][`pages`]);
-            bookStatus.append(myLibrary[i][`progress`]);
-            bookContainer.appendChild(bookTitle);
-            bookContainer.appendChild(bookAuthor);
-            bookContainer.appendChild(bookPages);
-            bookContainer.appendChild(bookStatus);
-            bookDisplay.appendChild(bookContainer);
-            console.log(bookContainer);
-        }
-        counter++;
+    if ((title != "") && (author != "") && (pages != "")){ //if values are not empty add book as new book
+        const newBook = new Book(title, author, pages, progress);
+        myLibrary.push(newBook);
+        console.log(myLibrary.length);
     }
-
-    
-    
+    let i = ((myLibrary.length) - 1); //same as mylibrary[index]
+    if (i >= 0){ //if it is not -1 (which means empty)
+        const bookContainer = document.createElement("div");
+        bookContainer.setAttribute("class", "books");
+        const bookTitle = document.createElement("p");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const bookStatus = document.createElement("p");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "delete";
+        bookTitle.append(myLibrary[i][`title`]);
+        bookAuthor.append(myLibrary[i][`author`]);
+        bookPages.append(myLibrary[i][`pages`]);
+        bookStatus.append(myLibrary[i][`progress`]);
+        bookContainer.appendChild(bookTitle);
+        bookContainer.appendChild(bookAuthor);
+        bookContainer.appendChild(bookPages);
+        bookContainer.appendChild(bookStatus);
+        bookContainer.appendChild(deleteBtn);
+        bookDisplay.appendChild(bookContainer);
+        console.log(bookContainer);
+    }    
 }
+    
 
 
 submitBook.addEventListener("click", () => {
